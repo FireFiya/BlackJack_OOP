ca42d43e203e35b55752bc1a368dc88c64a45b90
小黑 <126648922+FireFiya@users.noreply.github.com>
2023-05-24 17:34:39 +0800

    Delete Dealer.cpp

--- Dealer.cpp
+++
@@ -1,9 +0,0 @@
-//BlackJack(OOP) 0.1.4 - Dealer.cpp
-#include "Dealer.h"
-int Dealer::numOfCards = 0;
-int Dealer::getPoint()const {
-	return point;
-}
-Card Dealer::getCard()const {
-	return card;
-}
